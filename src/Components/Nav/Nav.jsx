0687f3d31a6cf441f6935f305e104e313a9b3629
sanjay121531 <71871677+sanjayloncha@users.noreply.github.com>
2023-03-01 23:59:12 +0530

    ui updated

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom" ;
+import { useNavigate } from "react-router-dom";
 
 import {
   Box,
@@ -20,32 +20,38 @@ import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 export default function Nav() {
   let [auth, setAuth] = useState(false);
-  let navigate = useNavigate() ;
+  let navigate = useNavigate();
   let login = () => {
     setAuth(true);
-    console.log("loggedIn") ;
-    navigate("/") ;
+    console.log("loggedIn");
+    navigate("/");
   };
-  let logout = ()=>{
-    setAuth(false) ;
-  }
+  let logout = () => {
+    setAuth(false);
+  };
   const { colorMode, toggleColorMode } = useColorMode();
-  let bg = useColorModeValue("yellow.200","yellow.500")
+  let bg = useColorModeValue("yellow.200", "yellow.500");
   return (
     <>
       <Box bg={useColorModeValue("yellow.200", "yellow.500")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <Box>
-            <lord-icon
+            {/* <lord-icon
               src="https://cdn.lordicon.com/wxnxiano.json"
               trigger="morph"
-              style={{"width":"50px","height":"100px"}}
+              style={{ width: "50px", height: "100px" }}
+            ></lord-icon> */}
+            <lord-icon
+              src="https://cdn.lordicon.com/hursldrn.json"
+              trigger="morph"
+              style={{ width: "50px", height: "100px" }}
+              // style="width:250px;height:250px"
             ></lord-icon>
           </Box>
 
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={7}>
-              <Button onClick={toggleColorMode} bg={bg} >
+              <Button onClick={toggleColorMode} bg={bg}>
                 {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
               </Button>
 
@@ -83,7 +89,7 @@ export default function Nav() {
                     <MenuDivider />
                     <MenuItem>Your Servers</MenuItem>
                     <MenuItem>Account Settings</MenuItem>
-                    <MenuItem onClick={logout} >Logout</MenuItem>
+                    <MenuItem onClick={logout}>Logout</MenuItem>
                   </MenuList>
                 </Menu>
               ) : (
