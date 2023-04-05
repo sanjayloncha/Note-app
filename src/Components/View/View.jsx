f5ee78b01c6a6aec3121ba1cffdaafac35ab6af4
sanjay121531 <71871677+sanjayloncha@users.noreply.github.com>
2023-04-05 22:12:57 +0530

    verified by signing new user

--- src/Components/View/View.jsx
+++ src/Components/View/View.jsx
@@ -1,14 +1,6 @@
 import React, { useEffect } from "react";
 
 export default function View() {
-  const getData = async () => {
-    let url = `https://note-app-data.onrender.com/users`;
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data[0].note) ;
-  };
-  useEffect(() => {}, []);
-  getData();
   return (
     <div>
       <h1>View component</h1>
